0ab39b462fca2bf61ec6edee15e6a990eae3007d
Ivan Lozic <ivanlozic995@gmail.com>
2023-12-18 16:21:49 +0100

    update profile service

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,11 +1,12 @@
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto implements Omit<CreateUserDto, 'friends'> {
-  readonly username: string;
-  readonly email: string;
-  readonly firstName: string;
-  readonly lastName: string;
-  readonly password: string;
-  readonly repeatPassword: string;
-  readonly mobileNumber: string;
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  repeatPassword: string;
+  mobileNumber: string;
 }
